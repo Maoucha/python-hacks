6e15cee82b559094db1e28ceef27852751d8e70d
Claire Amaouche <claire.pikae@gmail.com>
2016-10-11 22:21:15 +0200

    Generate a new file containing hashed items

--- encrytor.py
+++ encrytor.py
@@ -1,5 +1,6 @@
 # import Lib for encryting strings
 import hashlib
+import sys
 
 # Define functions
 def encryptMD5(string):
@@ -8,16 +9,11 @@ def encryptMD5(string):
 	return m.hexdigest()
 
 # read an input file
-input_file = open("test.csv", "r")	
+input_file = open(sys.argv[1], "r")
+ouput_file = open("output.csv", "w")
 line = input_file.readline()
-
 
 #read all the lines of the file one by one and hash them
 while line != "":
-	print encryptMD5(line[:-1])
+	ouput_file.write(encryptMD5(line[:-1]) + "\n")
 	line = input_file.readline()
-
-
-
-
-	
