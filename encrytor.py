ccbc54fe27373243cec75124bddf779306f65a2d
Claire Amaouche <claire.pikae@gmail.com>
2016-10-11 21:51:50 +0200

    Print file content

--- encrytor.py
+++ encrytor.py
@@ -7,4 +7,19 @@ def encryptMD5(string):
 	m.update(str(string).encode('utf-8'))
 	return m.hexdigest()
 
-print encryptMD5("bite")
+# read an input file
+input_file = open("test.csv", "r")	
+line = input_file.readline()
+
+# number of lines in the file
+index = 0
+
+#read all the lines of the file one by one
+while line != "":
+	index += 1
+	print str(index) + ": " + line[:-1]
+	line = input_file.readline()
+
+
+
+	
